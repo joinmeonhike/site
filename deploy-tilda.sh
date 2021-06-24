d5b3bd56ea94caabae1f544b71ca4aa15c802f9d
joinmeonhike <joinmeonhike@gmail.com>
2021-06-24 20:35:35 +0300

    Deploy

---
+++ deploy-tilda.sh
@@ -0,0 +1,20 @@
+echo "###"
+echo "Clear"
+echo "###"
+clear
+
+echo "###"
+echo "Building MeditationWebApp for tilda"
+echo "###"
+cd ../MeditationWebApp
+yarn build-for-tilda
+
+echo "###"
+echo "Copying MeditationWebApp for tilda"
+echo "###"
+mkdir ./map
+cp ../MeditationWebApp/dist/* ./
+
+git add .
+git commit -a -m "Deploy"
+git push
